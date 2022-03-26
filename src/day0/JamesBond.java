c424c9a055b6319f32b10f9fb5274e4a3e908747
Muradini <ataigdirov@gmail.com>
2022-03-26 13:50:15 -0700

    New updated class James Bond

--- src/day0/JamesBond.java
+++ src/day0/JamesBond.java
@@ -4,7 +4,7 @@ public class JamesBond{
 
     public static void main(String[] args) {
 
-        System.out.println("James Bond");
+        System.out.println("James Bond hacked our project");
 
     }
 }
